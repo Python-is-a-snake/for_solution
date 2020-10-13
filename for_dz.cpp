7e75b7410a93a9a9a379cdaf6cac66a6d572d5f6
Python-is-a-snake <45516701+Python-is-a-snake@users.noreply.github.com>
2020-10-13 10:53:16 +0300

    Update for_dz.cpp

--- for_dz.cpp
+++ for_dz.cpp
@@ -25,9 +25,26 @@ void triangle(){
         cout<<("\n");
     }
 }
+void isosceles_triangle(){
+    int e=1;
+    for(int a=1;a<=5;a++)
+    {
+        for(int b=4;b>=a;b--)
+        {
+            cout<<" ";
+        }
+        for(int c=0;c<e;c++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        e=e+2;
+    }
+}
+
 int main() {
     rectangle();
     triangle();
-    
+    isosceles_triangle();
     return 0;
 }
